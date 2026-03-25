d71e90ead78ce80c822ecf6b2a610af3a83887a9
pujaSingh05 <pujasingh2357@gmail.com>
2026-03-26 01:14:26 +0530

    Revert "setup and login page ui"
    This reverts commit fd26c8213a05483e0355b6f6b81b8070980daf2b.

--- android/app/src/main/kotlin/com/example/reddit_clone/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.reddit_clone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const logoPath = 'assets/images/logo.png';
-  static const googlePath = 'assets/images/google.png';
-  static const loginEmotePath = 'assets/images/login_emote.png';
-}
