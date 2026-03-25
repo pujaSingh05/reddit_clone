fd26c8213a05483e0355b6f6b81b8070980daf2b
Puja Singh <pujasingh@MacBook-Air-von-Puja.local>
2026-03-25 22:51:43 +0530

    setup and login page ui

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/firebase_options.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reddit Clone',
+      theme: Pallete.darkModeAppTheme,
+      home: const SizedBox(),
+    );
+  }
+}
+
